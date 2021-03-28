45cd168ae53ecfccf70e22c8c17e8da1c84ffe32
Austin Meyer <austinmeyer@Austins-MacBook-Pro.local>
2021-03-28 18:09:27 -0400

    Update data link.

--- grab_data.R
+++ grab_data.R
@@ -3,8 +3,8 @@ file.remove(this.file)
 
 tmp <- tempfile()
 data.folder <- paste('https://data.humdata.org/dataset/29d5f1e8-062e-4d30-be0c-bc50fab2a7c1/resource/4c934a8f-123c-45e3-8191-cfa8ce5c3158/download/')
-new.data.file <- paste('covid19_forecast_file_formatted_2021-02-26.zip', sep = '')
+new.data.file <- paste('covid19_forecast_file_formatted_2021-03-27.zip', sep = '')
 df.proj <- download.file(paste(data.folder, new.data.file, sep = ''), tmp)
 unzip(tmp, exdir = './')
 
-# df.g.proj <- read_csv('https://storage.googleapis.com/covid-external/forecast_COUNTY_14.csv')
+#https://data.humdata.org/dataset/29d5f1e8-062e-4d30-be0c-bc50fab2a7c1/resource/4c934a8f-123c-45e3-8191-cfa8ce5c3158/download/covid19_forecast_file_formatted_2021-03-27.zip
